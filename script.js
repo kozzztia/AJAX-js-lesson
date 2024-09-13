529529ff2c5189863d7b315318bc06be5b339292
kozzztia <kozzztia@gmail.com>
2024-09-13 11:27:31 +0300

    search button logic

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Project AJAX-js-lesson initialized")
+const searchButton = document.querySelector('.search-button')
+
+
+
+
+searchButton.onclick = (e) =>{
+    e.preventDefault()
+    console.log(e)
+    const searchValue = getSearchParams()
+
+    console.log(searchValue)
+}
+
+
+
+
+function getSearchParams(){
+    const value =  document.querySelector('.search-input').value;
+    if(value.length > 3){
+        return value;
+    }else{
+        document.querySelector('.search-input').focus()
+    }
+}
